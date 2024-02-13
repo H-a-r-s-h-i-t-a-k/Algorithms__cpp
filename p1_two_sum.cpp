8b779579944e58b687eb5e736ff20ec8f5c2bab0
Harshita <harshita@sample.com>
2024-02-13 13:07:37 +0530

    p1

---
+++ p1_two_sum.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        vector<int> final;
+        unordered_map<int, int> hm;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (hm.find(target - nums[i]) != hm.end())
+            {
+                final.push_back(hm[target - nums[i]]);
+                final.push_back(i);
+                return final;
+            }
+            hm[nums[i]] = i;
+        }
+        return final;
+    }
+};
+
+int main()
+{
+
+    return 1;
+}
