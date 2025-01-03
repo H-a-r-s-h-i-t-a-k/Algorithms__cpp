456ccd9db20e226f6ba27eda9285463538906007
HARSHITA <114668133+H-a-r-s-h-i-t-a-k@users.noreply.github.com>
2025-01-03 17:46:19 +0530

    03dd

---
+++ Heap/02_minimum_cost_ropes.cpp
@@ -0,0 +1,26 @@
+
+class Solution
+{
+public:
+    // Function to return the minimum cost of connecting the ropes.
+    int minCost(vector<int> &arr)
+    {
+        // code here
+        priority_queue<int, vector<int>, greater<int>> pq(arr.begin(), arr.end());
+
+        int res = 0;
+
+        while (pq.size() > 1)
+        {
+
+            int a = pq.top();
+            pq.pop();
+            int b = pq.top();
+            pq.pop();
+
+            res = res + a + b;
+            pq.push(a + b);
+        }
+        return res;
+    }
+};
