792e3a48de5f225fe522ff1f07ee4c6826d3599f
HARSHITA <114668133+H-a-r-s-h-i-t-a-k@users.noreply.github.com>
2025-01-04 15:37:41 +0530

    04

---
+++ leetcode/04_palindrome_subseq.cpp
@@ -0,0 +1,37 @@
+class Solution
+{
+public:
+    int countPalindromicSubsequence(string s)
+    {
+        unordered_set<char> element_list;
+        int ans = 0;
+        for (auto i : s)
+        {
+            element_list.insert(i);
+        }
+        for (auto el : element_list)
+        {
+            int i = -1, j = 0;
+
+            for (int k = 0; k < s.size(); k++)
+            {
+                if (s[k] == el)
+                {
+                    if (i == -1)
+                    {
+                        i = k;
+                    }
+                    j = k;
+                }
+            }
+
+            unordered_set<char> between;
+            for (int a = i + 1; a < j; a++)
+            {
+                between.insert(s[a]);
+            }
+            ans += between.size();
+        }
+        return ans;
+    }
+};
