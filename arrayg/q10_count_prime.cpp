919f15d08a0b083e3796c8d70cb0e4b4da21b9a8
HARSHITA <114668133+H-a-r-s-h-i-t-a-k@users.noreply.github.com>
2025-02-18 21:39:40 +0530

    done

--- arrayg/q10_count_prime.cpp
+++ arrayg/q10_count_prime.cpp
@@ -1,3 +1,28 @@
+
+// Sieve of Eratosthenes optime prime  count
+class Solution
+{
+public:
+    int countPrimes(int n)
+    {
+        vector<bool> arr(n + 1, true);
+        arr[0] = false;
+        arr[1] = false;
+        int c = 0;
+        for (int i = 2; i < n; i++)
+        {
+            if (arr[i])
+            {
+                c++;
+                for (int j = 2 * i; j < n; j = j + i)
+                {
+                    arr[j] = false;
+                }
+            }
+        }
+        return c;
+    }
+};
 class Solution
 {
 public:
